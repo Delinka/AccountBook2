64fcd86d972a84a33942cf0a23f56d4ea1540886
Delinka <275999907@qq.com>
2017-09-09 00:12:54 +0800

    加入左侧菜单的按钮

--- app/src/main/java/com/example/delinka/accountbook2/FragmentAccount.java
+++ app/src/main/java/com/example/delinka/accountbook2/FragmentAccount.java
@@ -2,15 +2,21 @@ package com.example.delinka.accountbook2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Delinka on 2017/9/8.
  */
 
 public class FragmentAccount extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_account_messege, container, false);
--- app/src/main/java/com/example/delinka/accountbook2/MainActivity.java
+++ app/src/main/java/com/example/delinka/accountbook2/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.delinka.accountbook2;
 
 import android.support.annotation.NonNull;
-import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.print.PrintHelper;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.security.PrivateKey;
 
-    private MenuItem menuItem;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public DrawerLayout drawerLayout;
+    private Button nav_button;
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -24,8 +30,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar_account_msg = (Toolbar) findViewById(R.id.toolbar_account_messege);
+        setSupportActionBar(toolbar_account_msg);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nav_button = (Button) findViewById(R.id.nav_button);
+        nav_button.setOnClickListener(this);
 
         replaceFragment(new FragmentAccount());
 
@@ -51,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.nav_button:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
 
     }
 
